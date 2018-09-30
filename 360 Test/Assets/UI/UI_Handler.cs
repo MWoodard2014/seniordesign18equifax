fea8798bfc36329f54d85d0f426c9147b9a1a638
Marcus Woodard <mbw0027@auburn.edu>
2018-09-30 18:35:11 -0500

    WIP on URL Pull
    WIP on URL Pull

--- 360 Test/Assets/UI/UI_Handler.cs
+++ 360 Test/Assets/UI/UI_Handler.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.Video;
+using UnityEngine.XR;
 
 public class UI_Handler : MonoBehaviour {
 
@@ -12,30 +14,33 @@ public class UI_Handler : MonoBehaviour {
 	public Image bannerButton1;
 	public Image bannerButton2;
 	public Text bannerButton1Text;
-	public Text bannerbutton2Text;
+	public Text bannerButton2Text;
 	public Button openButton;
 	public Button closeButton;
-
+	public GameObject GVREmu;
 
 	/*
 	// Variables regarding the VideoViewOn funtions
 	*/
 	public Canvas canvas;
 	public GameObject videoSphere;
+	private VideoPlayer videoPlayer;
+	private VideoSource videoSource;
+	public GvrReticlePointer reticlePointer;
 
 	//------------------------------------------------------------
 	// LeftBannerHandler and the associated IEnumerator handes the
-	// the sliding in and oout of the left side banner and the buttons
+	// the sliding in and out of the left side banner and the buttons
 	// on the left banner
 	//------------------------------------------------------------
 	public void LeftBannerHandler(){
-		if (navMenu.gameObject.active){
+		if (navMenu.gameObject.activeInHierarchy){
 			StartCoroutine(slideLeftBanner(-0.1f));
-		}else if (!navMenu.gameObject.active){
+			StartCoroutine(showLeftBannerButtons(0));
+		}else if (!navMenu.gameObject.activeInHierarchy){
 			navMenu.gameObject.SetActive(true);
 			StartCoroutine(slideLeftBanner(0.1f));
-			bannerButton1Text.gameObject.SetActive(true);
-			bannerbutton2Text.gameObject.SetActive(true);
+			StartCoroutine(showLeftBannerButtons(1));
 		}
 	}
 
@@ -50,6 +55,17 @@ public class UI_Handler : MonoBehaviour {
 			navMenu.gameObject.SetActive(false);
 	}
 
+	IEnumerator showLeftBannerButtons(int i){
+		if (i == 1){
+			yield return new WaitForSeconds(0.15f);
+			bannerButton1Text.gameObject.SetActive(true);
+			bannerButton2Text.gameObject.SetActive(true);
+		}else {
+			bannerButton1Text.gameObject.SetActive(false);
+			bannerButton2Text.gameObject.SetActive(false);
+		}
+	}
+
 	//------------------------------------------------------------
 	// VideoViewOn handles the removal of the canvas and appearance
 	// and beginning of the associated video.
@@ -57,5 +73,74 @@ public class UI_Handler : MonoBehaviour {
 	public void VideoViewOn(){
 		canvas.gameObject.SetActive(false);
 		videoSphere.gameObject.SetActive(true);
+		GVREmu.gameObject.SetActive(true);
+		StartCoroutine(playVideo());
+		// videoPlayer.gameObject.SetActive(true);
+		ToggleVR();
+		//StartCoroutine(GazeHandler());
+	}
+
+	// IEnumerator GazeHandler(){
+	// 	if (reticlePointer.TriggerDown){
+	// 		yield return new WaitForSeconds(2);
+	// 		if (reticlePointer.TriggerDown){
+	// 			Debug.Log("TriggerDown");
+	// 		}
+	// 	}
+	// }
+
+	IEnumerator playVideo()
+	{
+
+			//Add VideoPlayer to the GameObject
+			videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+
+			//Disable Play on Awake for both Video and Audio
+			videoPlayer.playOnAwake = false;
+
+			//Video clip from Url
+			videoPlayer.source = VideoSource.Url;
+			videoPlayer.url = "https://youtu.be/hsvsDTKrRS4";
+
+
+			//Set Audio Output to AudioSource
+			videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+			//Wait until video is prepared
+			while (!videoPlayer.isPrepared)
+			{
+					yield return null;
+			}
+
+			Debug.Log("Done Preparing Video");
+			 videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+			GetComponent<Renderer>().material.mainTexture = videoSphere.GetTexture();
+
+			//Play Video
+			videoPlayer.Play();
+
+			Debug.Log("Playing Video");
+			while (videoPlayer.isPlaying)
+			{
+					Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+					yield return null;
+			}
+
+			Debug.Log("Done Playing Video");
+	}
+
+	public void ToggleVR(){
+		if (XRSettings.loadedDeviceName == "cardboard"){
+				StartCoroutine(LoadDevice("None"));
+		} else {
+				StartCoroutine(LoadDevice("cardboard"));
+		}
+	}
+
+	IEnumerator LoadDevice(string newDevice){
+		XRSettings.LoadDeviceByName(newDevice);
+		yield return null;
+		XRSettings.enabled = true;
 	}
 }
